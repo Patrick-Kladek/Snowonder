13b0ef3f3f8aad98953e767f589fadafffe824b4
Patrick-Kladek <patrick.kladek@outlook.com>
2020-05-04 13:58:53 +0200

    remove unneeded appleScript file

--- Snowonder/ViewController.swift
+++ Snowonder/ViewController.swift
@@ -14,11 +14,6 @@ class ViewController: NSViewController {
 
     private struct Constant {
 
-        struct ScriptInfo {
-
-            static let openSystemPreferencesExtensions: Script.Info = (Bundle.main, "open_system_preferences_extensions", .scpt)
-        }
-
         struct URL {
 
             static let gitHub = Foundation.URL(string: "https://github.com/Karetski/Snowonder")
