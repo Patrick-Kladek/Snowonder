d27f3738bbf5549a7fa6b9fbe276929d0c6d38b5
Patrick-Kladek <patrick.kladek@outlook.com>
2020-05-04 13:58:10 +0200

    Fix open Extension Preferences pane

--- Snowonder/ViewController.swift
+++ Snowonder/ViewController.swift
@@ -9,38 +9,39 @@
 import Cocoa
 
 class ViewController: NSViewController {
-    
+
     // MARK: - Constant values
-    
+
     private struct Constant {
-        
+
         struct ScriptInfo {
-            
+
             static let openSystemPreferencesExtensions: Script.Info = (Bundle.main, "open_system_preferences_extensions", .scpt)
         }
-        
+
         struct URL {
-            
+
             static let gitHub = Foundation.URL(string: "https://github.com/Karetski/Snowonder")
         }
     }
-    
+
     // MARK: - Controller lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     // MARK: - Action handlers
-    
+
     @IBAction func enableExtensionButtonAction(_ sender: NSButton) {
-        Script(info: Constant.ScriptInfo.openSystemPreferencesExtensions).execute()
+        let url = URL(fileURLWithPath: "/System/Library/PreferencePanes/Extensions.prefPane")
+        NSWorkspace.shared.open(url)
     }
-    
+
     @IBAction func setupKeybindingsButtonAction(_ sender: NSButton) {
         // TODO: Implement in future versions
     }
-    
+
     @IBAction func gitHubButtonAction(_ sender: NSButton) {
         if let url = Constant.URL.gitHub {
             NSWorkspace.shared.open(url)
